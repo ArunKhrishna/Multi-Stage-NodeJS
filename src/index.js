ab69d7b0c537cb7166796a113dd88749e16cd7a9
arun <arunkhrishna@gmail.com>
2025-07-07 15:45:34 +0530

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// src/index.js
-
 const http = require("http");
+const chalk = require("chalk");
 
 const hostname = "0.0.0.0";
 const port = 3000;
@@ -8,9 +7,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("🚀 Hello from your Dockerized Node.js App!\n");
+  res.end(chalk.green("🚀 Hello from your Dockerized Node.js App!\n"));
 });
 
 server.listen(port, hostname, () => {
-  console.log(`✅ Server running at http://${hostname}:${port}/`);
+  console.log(chalk.blue(`✅ Server running at http://${hostname}:${port}/`));
 });
