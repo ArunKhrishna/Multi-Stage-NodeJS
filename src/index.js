0f37474f6367984d706ca48a5b0092a4c6215d19
arun <arunkhrishna@gmail.com>
2025-07-07 16:40:06 +0530

    kleur dependency added to package.json, replacing chalk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 const http = require("http");
+const kleur = require("kleur");
 
 const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http.createServer(async (req, res) => {
-  const chalk = await import('chalk');
-
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end(chalk.default.green("🚀 Hello from your Dockerized Node.js App!\n"));
+  res.end(kleur.green("🚀 Hello from your Dockerized Node.js App!\n"));
 });
 
 server.listen(port, hostname, () => {
-  import('chalk').then(chalk =>
-    console.log(chalk.default.blue(`✅ Server running at http://${hostname}:${port}/`))
-  );
+  console.log(kleur.blue(`✅ Server running at http://${hostname}:${port}/`));
 });
